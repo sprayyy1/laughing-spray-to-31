7b01bf0ea68774fa94fdabc991f736a77ff1f619
bariscodefx <bariscodefx@gmail.com>
2021-11-19 13:45:38 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int spgay;
+    
+   // fix 2021 by bariscodefx
+    //Microsoft hackleme kodu
+    printf("SPGAY: ");
+    scanf("%d",&spgay);
+    
+    if(spgay == 31)
+    {
+      printf("lol\n");
+    }
+    else{
+      printf("aptal\n");
+    }
+    return 0;
+}
